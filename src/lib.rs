46a284c98441ccba4cf2e3de090d518c99e1b3c4
jonathanxuu <1410101208@qq.com>
2022-05-31 18:11:30 +0800

    can consume more than 2 data to run the hash function, can run hash for multiple times according to the number of data

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
-use crypto::{Digest, ElementHasher, hash::rescue::rp64_256::ElementDigest};
+use crypto::{hash::rescue::rp64_256::ElementDigest, Digest, ElementHasher};
 // use log::debug;
-use sp_std::{ vec, vec::Vec};
+use sp_std::{vec, vec::Vec};
 // use serde::{Serialize, Deserialize};
 #[macro_use]
 extern crate alloc;
-use alloc::{ string::String};
+use alloc::string::String;
 // RE-EXPORTS
 // ================================================================================================
 
@@ -25,32 +25,51 @@ use hex;
 // ================================================================================================
 
 ///  Executes the `rescue` fucntion and returns the rescue hash result.
-/// 
+///
 /// * `inputs` specifies the rescue input, it should be a vec with 8 (u64) elements;
 /// *  Return the hash result Vec<u64>
 #[wasm_bindgen]
-pub fn rescue(values: String) -> Vec<u64>{
+pub fn rescue(values: String) -> Vec<u64> {
     let mut values_in_u64 = vec![];
-    if values.len() != 0{ 
+    if values.len() != 0 {
         let values_a: Vec<&str> = values.split(',').collect();
         values_in_u64 = values_a
-        .iter()
-        // .map(|public_a| public_a.parse::<u64>().unwrap())
-        .map(|public_a| public_a.parse::<u64>().expect("parse fail"))
-        .collect();
+            .iter()
+            // .map(|public_a| public_a.parse::<u64>().unwrap())
+            .map(|public_a| public_a.parse::<u64>().expect("parse fail"))
+            .collect();
     };
     assert!(
-        values_in_u64.len() == 8,
-        "expected len of values_in_u64 to be exactly 8 but received {}",
+        (values_in_u64.len()== 8) || ( (values_in_u64.len() > 8) && (values_in_u64.len() % 4 == 0)),
+        "expected len of values_in_u64 to be [exactly 8] or [over 4 but should be some multiple of 4] but received {}",
         values_in_u64.len()
     );
-    let elements = from_vec(values_in_u64);
-    let result = crypto::hashers::Rp64_256::hash_elements(&elements);
+    let mut elements = from_vec(values_in_u64);
+    console_log!("the elements are {:?}", elements);
+    let hash_times = if elements.len() == 8 {
+        1
+    } else {
+        elements.len() / 4 - 1
+    };
+    let mut result: ElementDigest;
+    if hash_times == 1 {
+        result = crypto::hashers::Rp64_256::hash_elements(&elements);
+    } else {
+        let mut first: Vec<BaseElement> = elements.drain(0..8).collect();
+        let mut to_be_hash = crypto::hashers::Rp64_256::hash_elements(&first);
+        for i in 1..hash_times {
+            let mut a = ElementDigest::digests_as_elements(&[to_be_hash]).to_vec();
+            let mut drain_4_element: Vec<BaseElement> = elements.drain(0..4).collect();
+            a.append(&mut drain_4_element);
+            to_be_hash = crypto::hashers::Rp64_256::hash_elements(&a);
+        }
+        result = to_be_hash;
+    };
 
-    return as_u64(result).to_vec()
+    return as_u64(result).to_vec();
 }
 
-pub fn as_u64(origin:ElementDigest) -> [u64; 4] {
+pub fn as_u64(origin: ElementDigest) -> [u64; 4] {
     let mut result = [0; 4];
     result[..1].copy_from_slice(&[origin.0[0].as_int()]);
     result[1..2].copy_from_slice(&[origin.0[1].as_int()]);
@@ -59,35 +78,34 @@ pub fn as_u64(origin:ElementDigest) -> [u64; 4] {
     result
 }
 
-pub fn rescue_two_para(para_1: [u64;4], para_2:[u64;4]) -> Vec<u64>{
+pub fn rescue_two_para(para_1: [u64; 4], para_2: [u64; 4]) -> Vec<u64> {
     let mut first: Vec<u64> = para_1.to_vec();
     let mut second: Vec<u64> = para_2.to_vec();
-    first.append(& mut second);
-
+    first.append(&mut second);
     let elements = from_vec(first);
     let result = crypto::hashers::Rp64_256::hash_elements(&elements);
-    return as_u64(result).to_vec()
+    return as_u64(result).to_vec();
 }
 
-pub fn rescue_two_para_u8_32(para_1: [u64;4], para_2:[u64;4]) -> [u8;32]{
+pub fn rescue_two_para_u8_32(para_1: [u64; 4], para_2: [u64; 4]) -> [u8; 32] {
     let mut first: Vec<u64> = para_1.to_vec();
     let mut second: Vec<u64> = para_2.to_vec();
-    first.append(& mut second);
+    first.append(&mut second);
 
     let elements = from_vec(first);
     let result = crypto::hashers::Rp64_256::hash_elements(&elements);
-    return result.as_bytes()
+    return result.as_bytes();
 }
 
 /// HELPER
-    pub fn from_vec(origin: Vec<u64>) -> [BaseElement;8]{
-        let mut res :Vec<BaseElement> = Vec::new();
-        for i in 0..8{
-            res.push(math::fields::f64::BaseElement::from(origin[i as usize]))
-        };
-        let result = res.try_into().unwrap();
-        return result;
+pub fn from_vec(origin: Vec<u64>) -> Vec<BaseElement> {
+    let mut res: Vec<BaseElement> = Vec::new();
+    for i in 0..origin.len() {
+        res.push(math::fields::f64::BaseElement::from(origin[i as usize]))
     }
+    let result = res.try_into().unwrap();
+    return result;
+}
 
 #[wasm_bindgen]
 pub fn init_panic_hook() {
@@ -97,16 +115,11 @@ pub fn init_panic_hook() {
 // GLOBAL CONSTANTS
 // ================================================================================================
 
-pub const MAX_CONTEXT_DEPTH : usize = 16;
-pub const MAX_LOOP_DEPTH    : usize = 8;
-
+pub const MAX_CONTEXT_DEPTH: usize = 16;
+pub const MAX_LOOP_DEPTH: usize = 8;
 
 #[test]
 fn hash_elements() {
     // let elements: [BaseElement; 8] = rand_array();
-    let elements1 = [134567439589739, 0, 0, 0];
-    let elements2 = [134567439589739, 0, 0, 0];
-    let result = rescue_two_para(elements1, elements2);
-    let expected_result:[u64;4] = [12201562084755916601, 14471853991255629693, 10239563256505788014, 13227819823987800438];
-    assert_eq!(result, expected_result);
+    let elements1 = String::from("1,2,3,4,5,6,7,8,1,2,3,4");
 }
